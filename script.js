e55f7fe7ff0a2ce56955da25818bfa95ba0bb839
UlaMielke <mielkeurszula92@gmail.com>
2018-04-17 11:50:53 +0200

    create script.js

---
+++ script.js
@@ -0,0 +1,65 @@
+
+var btnadd = document.querySelector(".add-btn-1");
+
+btnadd.addEventListener("click", function() {
+    var li = document.createElement("li");
+    var list = document.querySelector(".buy-list-1");
+    var text = document.querySelector(".add-value-1").value;
+    li.innerHTML = text;
+    list.appendChild(li);
+}, false)
+
+
+var btnadd = document.querySelector(".add-btn-2");
+
+btnadd.addEventListener("click", function() {
+    var li = document.createElement("li");
+    var list = document.querySelector(".buy-list-2");
+    var text = document.querySelector(".add-value-2").value;
+    li.innerHTML = text;
+    list.appendChild(li);
+}, false)
+
+
+var btnadd = document.querySelector(".add-btn-3");
+
+btnadd.addEventListener("click", function() {
+    var li = document.createElement("li");
+    var list = document.querySelector(".buy-list-3");
+    var text = document.querySelector(".add-value-3").value;
+    li.innerHTML = text;
+    list.appendChild(li);
+}, false)
+
+
+var btnadd = document.querySelector(".add-btn-4");
+
+btnadd.addEventListener("click", function() {
+    var li = document.createElement("li");
+    var list = document.querySelector(".buy-list-4");
+    var text = document.querySelector(".add-value-4").value;
+    li.innerHTML = text;
+    list.appendChild(li);
+}, false)
+
+
+var btnadd = document.querySelector(".add-btn-5");
+
+btnadd.addEventListener("click", function() {
+    var li = document.createElement("li");
+    var list = document.querySelector(".buy-list-5");
+    var text = document.querySelector(".add-value-5").value;
+    li.innerHTML = text;
+    list.appendChild(li);
+}, false)
+
+
+var btnadd = document.querySelector(".add-btn-6");
+
+btnadd.addEventListener("click", function() {
+    var li = document.createElement("li");
+    var list = document.querySelector(".buy-list-6");
+    var text = document.querySelector(".add-value-6").value;
+    li.innerHTML = text;
+    list.appendChild(li);
+}, false)
